c519d19700ef6d5499c023ef3f4103ba47eddbcc
YuliaHutsal <112818359+YuliaHutsal@users.noreply.github.com>
2023-04-10 20:07:52 +0300

    header finish

---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,18 @@
+import "./featured.css"
+import './'
+
+const Featured = () => {
+  return (
+    <div className="featured">
+        <div className="featuredItem">
+           <img src="" alt="" className="featuredImg" /> 
+        <div className="featuredTitles">
+            <h1>Dublin</h1>
+            <h2>123 properties</h2>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import Featured from "../../components/Featured/Featured";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Header/>
+        <div className="homeContainer">
+        <Featured/>
+        <Featured/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
